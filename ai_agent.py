3dea412c10326c1c9a863387aeead2b618a75a39
Piyush Kumar <piyushbhashkar@gmail.com>
2026-01-03 22:44:42 +0800

    first commit

---
+++ ai_agent.py
@@ -0,0 +1,14 @@
+
+# Phase1- Create AI Agent
+
+# 1.> Set up API keys for Groq and Tavily ( To power LLMs with lastest information)
+import os
+
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+TAVILY_API_KEY = os.environ.get("TAVILY_API_KEY")
+
+
+
+# 2.> Setup LLM and Tools
+# 3.> Set AI Agent with Search tool functionality 
